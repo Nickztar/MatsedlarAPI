f39b54d871d7556edfa02ebec7ad92c29b47b7cb
Nicke Broström <54627464+Nickztar@users.noreply.github.com>
2020-01-27 09:57:44 +0100

    beautfy

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-app.get('/', async(req,res)=>{
+app.get('/', async (req, res) => {
     await getData().then(response => {
         res.send(response);
     }).catch(err => {
@@ -16,18 +16,18 @@ app.get('/', async(req,res)=>{
 
 const url = 'https://webmenu.foodit.se/?r=13&m=1380&p=789&c=10120&w=0&v=Week&l=undefined';
 
-async function getData(){
+async function getData() {
     const foodData = [];
     await axios(url).then(response => {
         const html = response.data;
         const $ = cheerio.load(html);
         const foodList = $('#MenuRowContent > li');
-    
-        foodList.each(function(){
+
+        foodList.each(function () {
             const foods = [];
             const day = $(this).find('.block-a > strong').text();
             const date = $(this).find('.block-a > span').text();
-            $(this).find('.meal-text', '.dis-inline').each(function(){
+            $(this).find('.meal-text', '.dis-inline').each(function () {
                 foods.push($(this).text());
             });
             foods.reverse();
